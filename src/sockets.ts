8cd1d023697505cf5f52427ffb4b24a5a2ca73c5
JorLoCo <21460224@colima.tecnm.mx>
2025-04-03 16:27:59 -0600

    02/04/2025

--- src/sockets.ts
+++ src/sockets.ts
@@ -8,17 +8,26 @@ export function handleSockets(io: Server) {
             socket.emit("connected", {socketID: socket.id, message: "Te conectaste!" });
         }, 1000);
 
+        io.emit("message", {
+            socketID: "server",
+            message: `Bienvenido ${socket.id} al chat!`
+        });
+        const clientIp = socket.handshake.address.replace(/^.*:/, ''); // quitar prefijo ipv6;
+        const TimeMessage = socket.handshake.time
         socket.on("message", (data) => {
-            console.log(`${socket.id}:${ data }`);
+            console.log(`${socket.id}:${ data } desde ${clientIp} a la hora de ${TimeMessage}]`);
             io.emit("message",{
                 socketID: socket.id, 
                 message: data
-            }); // Enviar mensaje a todos
+            }); //Enviar mensaje a todos
         });
 
         socket.on("disconnect", () => {
             console.log("Usuario desconectado:", socket.id);
+            io.emit("message", {
+                socketID: "server",
+                message: `Adios ${socket.id}, que le vaya bien en el bote`
+            });
         });
     });
-
 }
